394913eb145ed5d6808cc8d12b30559a79c827ac
rajagceian <raja.gce.gce@gmail.com>
2026-07-12 19:29:03 +0530

    Add introductory comments about Node JS and its capabilities

---
+++ Introduction/aboutNodeJS.js
@@ -0,0 +1,16 @@
+//Node JS kya hai?
+//Hum jante hai ki JavaScript ek programming language hai jo ki web development me use hoti hai. 
+// Lekin JavaScript sirf browser ke andar hi run hoti thi. 
+
+//--- To yahi par Sawal uthta hai ki agar hum JavaScript ko browser ke bahar run karna chahte hai to 
+// hum kya karenge?
+
+//--- To is problem ka solution hai Node JS.    
+// Node JS ek open-source, cross-platform, JavaScript runtime environment hai jo ki server-side aur 
+// networking applications ke liye use hota hai.
+
+//--- Node JS V8 Engine ko C++ ke saath integrate karke JavaScript ko browser ke bahar run karne ki 
+// ability provide karta hai.
+
+//--- So, Ab C++ ke tarah hum Node js ke through bhi file handling, database handling, networking aur 
+// server side programming kar sakte hai.
